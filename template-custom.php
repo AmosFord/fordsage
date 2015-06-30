9f4e48f9db98b4f45439713cc389b2691b04def5
root <root@www.amosford.com>
2015-06-30 11:25:06 -0400

    First commit

--- template-custom.php
+++ template-custom.php
@@ -4,7 +4,59 @@
  */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<div class="container">
+    
+<div class="col-sm-12 text-center intro-text" >
+    <h1>Hey!! I'm Amos Ford. Some would call me a ...</h1>
+</div>
+
+<div class="col-sm-12 titles">
+	<div class="hidden-xs col-sm-4 col-md-4 col-md-offset-1 col-lg-3 col-lg-offset-2">
+		<div class="col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4">
+			<div id="profile-photo" class="profile-photo-2 col-sm-6 col-sm-offset-3"></div>
+		</div>
+	</div> 
+	<div class="col-sm-8 col-md-7 col-lg-5">   
+    		<div id="first-job-title" class="col-sm-12 text-center">
+        		<div class="col-sm-12"><p><h3>Front-End Developer</h3></p></div>
+    		</div>
+   	 	<div id="second-job-title" class="col-sm-12 text-center">
+        		<div class="col-sm-12"><p><h3>Innovative Solutionist</h3></p></div>
+    		</div>
+   		<div id="third-job-title" class="col-sm-12 text-center">
+        		<div class="col-sm-12"><p><h3>Avid Cereal Enthusiast</h3></p></div>
+    		</div>
+ 	</div>
+</div>
+<div id="coming-soon-text" class="col-sm-12 text-section text-center">   
+    <p><h4>I would describe myself as a guy enjoying the early stages of his career. I believe in efficient work, creative solutions, and an agile lifestyle.</p>
+    <p>This site is still under development. Find out more about me by visiting one of my other sites. </h4></p>
+</div>
+    
+<div class="col-sm-12 text-center icon-section">
+    	<div id="github-home-svg" class="col-sm-4 text-center">
+		<a href="https://github.com/AmosFord" target="_blank">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/github-icon-svg.svg" alt="Smiley face" height="60" width="60"  class="svg">
+			<p><strong>GitHub</strong></p>		
+		</a>
+    	</div>
+	<div id="linkedin-home-svg" class="col-sm-4  text-center">
+		<a href="https://www.linkedin.com/in/amosford" target="_blank">
+			<img src="<?php echo bloginfo('stylesheet_directory'); ?>/assets/images/linkedin-icon-svg.svg" alt="Smiley face" height="60" width="60" class="svg">
+			<p><strong>LinkedIn</strong></p>
+		</a>
+    	</div>
+    	<div id="facebook-home-svg" class="col-sm-4 text-center">
+		<a href="https://www.facebook.com/aceakahighdepth" target="_blank">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/facebook-icon-svg.svg" alt="Smiley face" height="60" width="60"  class="svg">
+			<p><strong>FaceBook</strong></p>
+		</a>
+    	</div>  
+
+</div>
+
+<div class="col-sm-12  text-section text-center">
+	<h4>"The difference between theory and practice is smaller in theory than in practice."<br> –  Roger Pate </h4>
+</div>
+
+</div>
