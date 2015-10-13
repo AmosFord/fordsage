bdc8151b1aa8d6b0fd931bb998db54fe294c2e2e
Amos Ford <amos.ford@synechron.com>
2015-10-13 12:14:48 -0400

    Added new template file and images

---
+++ template-about.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: About Template
+ */
+?>
+
+<div class="jumbotron jumbotron-about text-center">
+	<h2>Read ahead and find out</h2>
+	<h1>Who I Am</h1>
+	<h2></h2>
+    <h3></h3>
+</div>
+
+<div class="col-sm-12 text-center icon-section text-center">
+    	<div id="github-home-svg" class="col-sm-4 ">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/projectmanagericon.png" alt="Smiley face"  class="img-responsive">
+			<h4>Product Management</h4>		
+    	</div>
+	<div id="linkedin-home-svg" class="col-sm-4 ">
+			<img src="<?php echo bloginfo('stylesheet_directory'); ?>/assets/images/webdevelopmenticon.png" alt="Smiley face"  class="img-responsive">
+			<h4>Web Development</h4>
+    	</div>
+    	<div id="facebook-home-svg" class="col-sm-4 ">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/Problemsolvingicon.png" alt="Smiley face" class="img-responsive">
+			<h4>Innovative Consultant</h4>
+    	</div>  
+
+</div>
+
+<div class="col-sm-12 text-center intro-text" >
+    <h3>A brief background</h3>
+    <div class="col-sm-8 col-sm-offset-2">
+	    <p> My interest in software development stemmed from my passion for technology. Between developing websites and checking out the latest technology, I spend most
+	     of my time in front of the computer. Some of my passions include writting clean code, creating modern user experiences and listening to great music.</p>
+	    <p>This site is still under development. Contact me to find out more. </p>
+    </div>
+</div>
+    
+<div class="col-sm-12 text-center icon-section">
+
+
+    	<div id="github-home-svg" class="col-sm-4 text-center">
+		<a href="https://github.com/AmosFord" target="_blank">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/github-icon-svg.svg" alt="Smiley face" height="60" width="60"  class="svg">
+			<p><strong>GitHub</strong></p>		
+		</a>
+    	</div>
+	<div id="linkedin-home-svg" class="col-sm-4  text-center">
+		<a href="https://www.linkedin.com/in/amosford" target="_blank">
+			<img src="<?php echo bloginfo('stylesheet_directory'); ?>/assets/images/linkedin-icon-svg.svg" alt="Smiley face" height="60" width="60" class="svg">
+			<p><strong>LinkedIn</strong></p>
+		</a>
+    	</div>
+    	<div id="facebook-home-svg" class="col-sm-4 text-center">
+		<a href="https://www.facebook.com/aceakahighdepth" target="_blank">
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/facebook-icon-svg.svg" alt="Smiley face" height="60" width="60"  class="svg">
+			<p><strong>FaceBook</strong></p>
+		</a>
+    	</div>  
+
+    	<div class="col-sm-12 text-center">
+			<p>"The difference between theory and practice is smaller in theory than in practice."<br> –  Roger Pate </p>
+		</div>
+
+</div>
+
+
+
